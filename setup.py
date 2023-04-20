50b052196b8d9b5d6606f18a01e1db71521c772c
Christopher Marchfelder <marchfelder@gmail.com>
2023-04-20 19:27:11 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="VHS Banking 1.0",
+    version="0.1",
+    author="Christopher Marchfelder",
+    author_email="marchfelder@gmail.com",
+    description="Example project for the advanced community college course"
+)
